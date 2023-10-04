f1c9deac94d3a8e5a3411f493d9816914ad8ff2b
aldanin <od120162@gmail.com>
2023-10-04 13:21:17 +0300

    Added abort command, updated key-bindings to key-padd area, cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,7 @@ enum  BooleanSetting {
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
+  let areCommandsRunning: boolean = false;
 
   if (getBooleanSettingState(BooleanSetting.createOnStartup)) {
     createTerminals();
@@ -42,134 +43,111 @@ export async function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 
-  // TODO - Currently not applied - no solution how to send ctrl+c to console
-  // disposable = vscode.commands.registerCommand(
-  //   "terminals-control.abortAll",
-  //   abortAllRunningCommands
-  // );
+  disposable = vscode.commands.registerCommand(
+    "terminals-control.abortAll",
+    abortAllRunningCommands
+  );
 
-  // context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+
+  function createTerminals() {
+    let customTerminals: vscode.Terminal[] | undefined;
+  
+    if (getBooleanSettingState(BooleanSetting.resetBeforeCreate)) {
+      vscode.window.terminals?.forEach((terminal) => terminal.dispose());
+    }
+  
+    const configedTerminals: TerminalConfig[] | undefined =
+      getConfigedTerminalList();
+  
+    if (!configedTerminals) {
+      return;
+    }
+  
+    // const settings = await vscode.workspace.fs.readFile(vscode.Uri.parse('%APPDATA%\\Roaming\\Code\\User\\settings.json'));
+  
+    customTerminals = configedTerminals?.map((terminalConfig) => {
+      const { name, color, iconPath } = terminalConfig;
+      const newColor = `terminal.ansi${
+        color.charAt(0).toUpperCase() + color.slice(1)
+      }`;
+  
+      const terminal = vscode.window.createTerminal({
+        name,
+        color: new vscode.ThemeColor(newColor),
+        iconPath: new vscode.ThemeIcon(iconPath),
+      });
+  
+      return terminal;
+    });
+  
+    customTerminals?.forEach((customTerminal) => customTerminal.show());
+    
+    areCommandsRunning = false;
+  }
+  
+  function killTerminals() {
+    vscode.window.terminals?.forEach((terminal) => terminal.dispose());
+    vscode.window.createTerminal().show();
+    
+    areCommandsRunning = false;
+  }
+  
+  function runAllRegisteredCommands() {
+    if (areCommandsRunning) {
+      return;
+    }
+
+    const configedTerminals = getConfigedTerminalList();
+  
+    configedTerminals?.forEach((configedTerminal) => {
+      const terminal: vscode.Terminal | undefined = vscode.window.terminals.find(
+        (terminal) => terminal.name === configedTerminal.name
+      );
+  
+      configedTerminal.command && terminal?.sendText((<unknown>configedTerminal.command) as string, true);
+    });
+
+    areCommandsRunning = true;
+  }
+  
+  async function abortAllRunningCommands() {
+    if (!areCommandsRunning) {
+      return;
+    }
+
+    createTerminals();
+
+    areCommandsRunning = false;
+
+  }
+  
+  ///////////////////////////
+  
+  function getConfigedTerminalList(): TerminalConfig[] | undefined {
+    const terminalsControl: vscode.WorkspaceConfiguration =
+      vscode.workspace.getConfiguration("terminals-control");
+  
+    if (!terminalsControl) {
+      return undefined;
+    }
+  
+    const configedTerminals: TerminalConfig[] | undefined =
+      terminalsControl.get<TerminalConfig[]>("terminals");
+  
+    return configedTerminals;
+  }
+  
+  function getBooleanSettingState(booleanSetting: BooleanSetting): boolean {
+    const terminalsControl: vscode.WorkspaceConfiguration =
+      vscode.workspace.getConfiguration("terminals-control");
+  
+    const isTrue: boolean =
+      terminalsControl.get<boolean>(booleanSetting) ?? true;
+  
+    return isTrue;
+  }
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
-
-function createTerminals() {
-  let customTerminals: vscode.Terminal[] | undefined;
-
-  if (getBooleanSettingState(BooleanSetting.resetBeforeCreate)) {
-    vscode.window.terminals?.forEach((terminal) => terminal.dispose());
-  }
-
-  const configedTerminals: TerminalConfig[] | undefined =
-    getConfigedTerminalList();
-
-  if (!configedTerminals) {
-    return;
-  }
-
-  // const settings = await vscode.workspace.fs.readFile(vscode.Uri.parse('%APPDATA%\\Roaming\\Code\\User\\settings.json'));
-
-  customTerminals = configedTerminals?.map((terminalConfig) => {
-    const { name, color, iconPath } = terminalConfig;
-    const newColor = `terminal.ansi${
-      color.charAt(0).toUpperCase() + color.slice(1)
-    }`;
-
-    const terminal = vscode.window.createTerminal({
-      name,
-      color: new vscode.ThemeColor(newColor),
-      iconPath: new vscode.ThemeIcon(iconPath),
-    });
-
-    return terminal;
-  });
-
-  customTerminals?.forEach((customTerminal) => customTerminal.show());
-}
-
-function killTerminals() {
-  vscode.window.terminals?.forEach((terminal) => terminal.dispose());
-  vscode.window.createTerminal().show();
-}
-
-function runAllRegisteredCommands() {
-  const configedTerminals = getConfigedTerminalList();
-
-  configedTerminals?.forEach((configedTerminal) => {
-    const terminal: vscode.Terminal | undefined = vscode.window.terminals.find(
-      (terminal) => terminal.name === configedTerminal.name
-    );
-
-    configedTerminal.command && terminal?.sendText((<unknown>configedTerminal.command) as string, true);
-  });
-}
-
-async function abortAllRunningCommands() {
-  const configedTerminals = getConfigedTerminalList();
-
-  configedTerminals?.forEach((configedTerminal) => {
-    if (configedTerminal.command) {
-      const terminal: vscode.Terminal | undefined =
-        vscode.window.terminals.find(
-          (terminal) => terminal.name === configedTerminal.name
-        );
-
-      terminal?.processId.then((PID) => {
-        if (!PID) {
-          return;
-        }
-
-        const killCOmmand = `taskkill /F /PID ${PID}`;
-
-        terminal?.sendText(killCOmmand, true);
-      });
-    }
-  });
-}
-
-///////////////////////////
-
-function getConfigedTerminalList(): TerminalConfig[] | undefined {
-  const terminalsControl: vscode.WorkspaceConfiguration =
-    vscode.workspace.getConfiguration("terminals-control");
-
-  if (!terminalsControl) {
-    return undefined;
-  }
-
-  const configedTerminals: TerminalConfig[] | undefined =
-    terminalsControl.get<TerminalConfig[]>("terminals");
-
-  return configedTerminals;
-}
-
-function getBooleanSettingState(booleanSetting: BooleanSetting): boolean {
-  const terminalsControl: vscode.WorkspaceConfiguration =
-    vscode.workspace.getConfiguration("terminals-control");
-
-  const isTrue: boolean =
-    terminalsControl.get<boolean>(booleanSetting) ?? true;
-
-  return isTrue;
-}
-
-// "terminals-control.terminals": [
-//     { "name": "ugs", "color": "green", "iconPath": "debug-start" },
-//     { "name": "mccx", "color": "green", "iconPath": "debug-start" },
-//     {
-//       "name": "git",
-//       "color": "blue",
-//       "iconPath": "source-control-view-icon"
-//     },
-//     {
-//       "name": "reflog",
-//       "color": "blue",
-//       "iconPath": "source-control-view-icon"
-//     },
-//     {
-//       "name": "yarn",
-//       "color": "red",
-//       "iconPath": "source-control-view-icon"
-//     }
-//   ]
